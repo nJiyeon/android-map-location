e84a075d8c45225c512904004fe97e285a4f4b45
jiyeon <chxistinn@gmail.com>
2024-07-21 13:47:13 +0900

    Step1 피드백 내용 반영

--- app/src/main/java/campus/tech/kakao/map/viewmodel/main/MainViewModelFactory.kt
+++ app/src/main/java/campus/tech/kakao/map/viewmodel/main/MainViewModelFactory.kt
@@ -9,3 +9,6 @@ class MainViewModelFactory(private val application: Application) : ViewModelProv
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             return MainViewModel(application) as T
         }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
